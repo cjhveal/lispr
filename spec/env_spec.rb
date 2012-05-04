2af7e7d0f8929d6e377cdb11962ac590c69a68eb
Cody Veal <cveal05@gmail.com>
2012-05-04 00:12:50 -0400

    additional environment test cases

--- spec/env_spec.rb
+++ spec/env_spec.rb
@@ -29,4 +29,18 @@ describe Env do
   it 'should not set an undefined symbol' do
     lambda { @env.set!(:y, 1) }.should raise_error
   end
+
+  context "when in local environment" do
+    before(:each) do
+      @local_env = Env.new Hash[:y, 2], @env
+    end
+
+    it 'should find local variables' do
+      @local_env.find(:y).should eq 2
+    end
+
+    it 'should find global variables' do
+      @local_env.find(:x).should eq 1
+    end
+  end
 end
